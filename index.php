90b4a51a12b972951ca2627ad2849b5a45433b8f
jung_jung <kiki128qk@naver.com>
2021-03-09 18:40:26 +0900

    03-09 헤더 디자인중

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My page</title>
+
+    <!-- fontawesome link -->
+    <script src="https://kit.fontawesome.com/9e31582c84.js" crossorigin="anonymous"></script>
+    <!-- plugin css link -->
+    <link rel="stylesheet" href="/mainPofo/plugin/lightslider.css" />
+
+    <!-- style css link -->
+    <link rel="stylesheet" href="/mainPofo/css/reset.css" />
+    <link rel="stylesheet" href="/mainPofo/css/style.css" />
+
+</head>
+<body>
+    <div class="wrap">
+        <header>
+            <div class="headerBoxs">
+                <div class="mainBoxs">
+                    <div class="boxL">
+                        <div class="gBox"></div>
+                        <div class="Ybox"></div>
+                    </div>
+                    <div class="boxR">
+                        <h1>Draw up a<br>Draft Design</h1>
+                    </div>
+                </div>
+
+                <div class="WhiteBoxs">
+                    <p class="wBoxT"></p>
+                    <p class="wBoxB"></p>
+                </div>
+            </div>
+        </header> 
+        
+    </div>
+
+    <!-- jquery link -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</body>
+</html>
