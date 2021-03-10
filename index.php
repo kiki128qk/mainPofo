1e427512e159a912148606cfb0ba17fca4267105
jung_jung <kiki128qk@naver.com>
2021-03-10 18:40:16 +0900

    03-10 헤더 완성 중앙 컨텐츠 제작중

--- index.php
+++ index.php
@@ -20,22 +20,106 @@
     <div class="wrap">
         <header>
             <div class="headerBoxs">
-                <div class="mainBoxs">
-                    <div class="boxL">
-                        <div class="gBox"></div>
-                        <div class="Ybox"></div>
-                    </div>
-                    <div class="boxR">
-                        <h1>Draw up a<br>Draft Design</h1>
-                    </div>
-                </div>
-
-                <div class="WhiteBoxs">
+                <div class="whiteBoxs">
+                    <p class="gBox"></p>   
                     <p class="wBoxT"></p>
                     <p class="wBoxB"></p>
                 </div>
+                <div class="mainBoxs">
+                    <div class="boxL">
+                        <h1>panton<br>color<br>2021</h1>
+                        <svg version="1.1" id="panBox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"y="0px" viewBox="0 0 450 450" style="enable-background:new 0 0 450 450;" xml:space="preserve">
+                        <g>
+                            <rect x="23.5" y="20" class="st0" width="257.5" height="254"/>
+                            <path d="M276.5,25v248.3h-250V25H276.5 M301.5,0H1.5v298.3h300V0L301.5,0z"/>
+                        </g>
+                        <g>
+                            <path d="M425.5,176.7V425h-250V176.7H425.5 M450.5,151.7h-300V450h300V151.7L450.5,151.7z"/>
+                        </g>
+                        <g>
+                            <rect x="150.5" y="151.7" width="151" height="146.7"/>
+                        </g>
+                    </svg>
+                    </div>
+                    <div class="boxR">
+                        <h2>Draw up a<br>Draft Design</h2>
+                    </div>
+                </div>
             </div>
-        </header> 
+        </header>
+        <Section class="contents">
+                
+                <div class="content">
+                    <p class="designBoxL"></p>
+                    <p class="designBoxR"></p>
+                    <div class="leftBox">
+                        <div class="imgtitle">
+                            <h3 class="title">Re Make</h3>
+                            <span></span>
+                        </div>
+                        <div class="imgBox">
+                            <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
+                        </div>
+                    </div>
+
+                    <div class="rightBox">
+                        <div class="txtcon">
+                            <h3>01</h3>
+                            <em>Title: . 인생을 관현악이며,<br>
+                            Use Language:오아이스도 위하여 <br>
+아름답고 미인을 동력은 있으며, 원대하고, 청춘의 청춘이 그들은 동산에는 것이다 물방아 눈이 부패뿐이다.  아름다우냐?</em>
+                        </div>
+                        <button>자세히 보기</button>
+                    </div>
+                </div>
+                <div class="content ">
+                    <p class="designBoxL"></p>
+                    <div class="leftBox">
+                        <div class="imgtitle">
+                            <h3 class="title">Re Make</h3>
+                            <span></span>
+                        </div>
+                        <div class="imgBox">
+                            <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
+                        </div>
+                    </div>
+
+                    <div class="rightBox">
+                        <div class="txtcon">
+                            <h3>02</h3>
+                            <em>Title: . 인생을 관현악이며,<br>
+                            Use Language:오아이스도 위하여 <br>
+아름답고 미인을 동력은 있으며, 원대하고, 청춘의 청춘이 그들은 동산에는 것이다 물방아 눈이 부패뿐이다.  아름다우냐?</em>
+                        </div>
+                        <button>자세히 보기</button>
+                    </div>
+                </div>
+                
+                <div class="content ">
+                    <p class="designBoxL"></p>
+                    <p class="designBoxR Thirdcon"></p>
+                    <div class="leftBox">
+                        <div class="imgtitle">
+                            <h3 class="title">Re Make</h3>
+                            <span></span>
+                        </div>
+                        <div class="imgBox">
+                            <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
+                        </div>
+                    </div>
+
+                    <div class="rightBox">
+                        <div class="txtcon">
+                            <h3>03</h3>
+                            <em>Title: . 인생을 관현악이며,<br>
+                            Use Language:오아이스도 위하여 <br>
+아름답고 미인을 동력은 있으며, 원대하고, 청춘의 청춘이 그들은 동산에는 것이다 물방아 눈이 부패뿐이다.  아름다우냐?</em>
+                        </div>
+                        <button>자세히 보기</button>
+                    </div>
+                </div>
+                
+        </Section>
         
     </div>
 
