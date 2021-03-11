1290867aa0645ce498f4e64e0671bfe7bac7b4a6
jung_jung <kiki128qk@naver.com>
2021-03-11 18:41:27 +0900

    03-11 메인컨텐츠 완료 Moretap 제작중

---
+++ include/header.php
@@ -0,0 +1,26 @@
+               
+ <div class="whiteBoxs">
+                    <p class="gBox"></p>   
+                    <p class="wBoxT"></p>
+                    <p class="wBoxB"></p>
+                </div>
+                <div class="mainBoxs">
+                    <div class="boxL">
+                    <h1>panton<br>color<br>2021</h1>
+                    <svg version="1.1" id="panBox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"y="0px" viewBox="0 0 450 450" style="enable-background:new 0 0 450 450;" xml:space="preserve">
+                    <g>
+                        <rect x="23.5" y="20" class="st0" width="257.5" height="254"/>
+                        <path d="M276.5,25v248.3h-250V25H276.5 M301.5,0H1.5v298.3h300V0L301.5,0z"/>
+                    </g>
+                    <g>
+                        <path d="M425.5,176.7V425h-250V176.7H425.5 M450.5,151.7h-300V450h300V151.7L450.5,151.7z"/>
+                    </g>
+                    <g>
+                        <rect x="150.5" y="151.7" width="151" height="146.7"/>
+                    </g>
+                    </svg>
+                    </div>
+                    <div class="boxR">
+                        <h2>Draw up a<br>Draft Design</h2>
+                    </div>
+                </div>
--- index.php
+++ index.php
@@ -8,9 +8,10 @@
 
     <!-- fontawesome link -->
     <script src="https://kit.fontawesome.com/9e31582c84.js" crossorigin="anonymous"></script>
-    <!-- plugin css link -->
-    <link rel="stylesheet" href="/mainPofo/plugin/lightslider.css" />
-
+    <!-- Plugin Aos link -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"> 
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> 
+    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script> 
     <!-- style css link -->
     <link rel="stylesheet" href="/mainPofo/css/reset.css" />
     <link rel="stylesheet" href="/mainPofo/css/style.css" />
@@ -19,107 +20,104 @@
 <body>
     <div class="wrap">
         <header>
+        <script> 
+            AOS.init(); 
+        </script>   
             <div class="headerBoxs">
-                <div class="whiteBoxs">
-                    <p class="gBox"></p>   
-                    <p class="wBoxT"></p>
-                    <p class="wBoxB"></p>
-                </div>
-                <div class="mainBoxs">
-                    <div class="boxL">
-                        <h1>panton<br>color<br>2021</h1>
-                        <svg version="1.1" id="panBox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"y="0px" viewBox="0 0 450 450" style="enable-background:new 0 0 450 450;" xml:space="preserve">
-                        <g>
-                            <rect x="23.5" y="20" class="st0" width="257.5" height="254"/>
-                            <path d="M276.5,25v248.3h-250V25H276.5 M301.5,0H1.5v298.3h300V0L301.5,0z"/>
-                        </g>
-                        <g>
-                            <path d="M425.5,176.7V425h-250V176.7H425.5 M450.5,151.7h-300V450h300V151.7L450.5,151.7z"/>
-                        </g>
-                        <g>
-                            <rect x="150.5" y="151.7" width="151" height="146.7"/>
-                        </g>
-                    </svg>
-                    </div>
-                    <div class="boxR">
-                        <h2>Draw up a<br>Draft Design</h2>
-                    </div>
-                </div>
+            <?php
+                include $_SERVER['DOCUMENT_ROOT'].'/mainpofo/include/header.php';
+            ?>
             </div>
         </header>
-        <Section class="contents">
-                
-                <div class="content">
-                    <p class="designBoxL"></p>
-                    <p class="designBoxR"></p>
-                    <div class="leftBox">
-                        <div class="imgtitle">
-                            <h3 class="title">Re Make</h3>
+        <Section class="contents">  
+            <div class="content frestCon">
+                <p class="designBoxL"></p>
+                <p class="designBoxR"></p>
+                <div class="imgBox">
+                    <div class="imgtitle">
+                        <h3 class="title">Re Make</h3>
                             <span></span>
-                        </div>
-                        <div class="imgBox">
-                            <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
-                        </div>
                     </div>
-
-                    <div class="rightBox">
-                        <div class="txtcon">
-                            <h3>01</h3>
-                            <em>Title: . 인생을 관현악이며,<br>
-                            Use Language:오아이스도 위하여 <br>
-아름답고 미인을 동력은 있으며, 원대하고, 청춘의 청춘이 그들은 동산에는 것이다 물방아 눈이 부패뿐이다.  아름다우냐?</em>
-                        </div>
-                        <button>자세히 보기</button>
+                    <div class="imgpart" data-aos="zoom-in">
+                        <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
                     </div>
                 </div>
-                <div class="content ">
-                    <p class="designBoxL"></p>
-                    <div class="leftBox">
-                        <div class="imgtitle">
-                            <h3 class="title">Re Make</h3>
-                            <span></span>
-                        </div>
-                        <div class="imgBox">
-                            <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
-                        </div>
-                    </div>
 
-                    <div class="rightBox">
-                        <div class="txtcon">
-                            <h3>02</h3>
-                            <em>Title: . 인생을 관현악이며,<br>
+                <div class="txtBox">
+                    <div class="txtcon"  data-aos="fade-right">
+                        <h3>01</h3>
+                        <em>Title: . 인생을 관현악이며,<br>
                             Use Language:오아이스도 위하여 <br>
 아름답고 미인을 동력은 있으며, 원대하고, 청춘의 청춘이 그들은 동산에는 것이다 물방아 눈이 부패뿐이다.  아름다우냐?</em>
-                        </div>
-                        <button>자세히 보기</button>
+                    </div>
+                    <a href="#"> <button Type="button">자세히 보기</button></a>
+                </div>
+            </div>
+            <div class="content secondCon">
+                <p class="designBoxL"></p>
+                <div class="imgBox">
+                    <div class="imgtitle">
+                        <h3 class="title">Re Make</h3>
+                        <span></span>
+                    </div>
+                    <div class="imgpart" data-aos="zoom-in">
+                        <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
                     </div>
                 </div>
-                
-                <div class="content ">
-                    <p class="designBoxL"></p>
-                    <p class="designBoxR Thirdcon"></p>
-                    <div class="leftBox">
-                        <div class="imgtitle">
-                            <h3 class="title">Re Make</h3>
-                            <span></span>
-                        </div>
-                        <div class="imgBox">
-                            <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
-                        </div>
-                    </div>
 
-                    <div class="rightBox">
-                        <div class="txtcon">
-                            <h3>03</h3>
-                            <em>Title: . 인생을 관현악이며,<br>
+                <div class="txtBox">
+                    <div class="txtcon" data-aos="fade-right">
+                        <h3>02</h3>
+                        <em>Title: . 인생을 관현악이며,<br>
                             Use Language:오아이스도 위하여 <br>
 아름답고 미인을 동력은 있으며, 원대하고, 청춘의 청춘이 그들은 동산에는 것이다 물방아 눈이 부패뿐이다.  아름다우냐?</em>
-                        </div>
-                        <button>자세히 보기</button>
+                    </div>
+                    <a href="#"> <button Type="button">자세히 보기</button></a>
+                </div>
+            </div>
+                
+            <div class="content thirdCon">
+                <p class="designBoxL"></p>
+                <p class="designBoxR "></p>
+                <div class="imgBox">
+                    <div class="imgtitle">
+                        <h3 class="title">Re Make</h3>
+                        <span></span>
+                    </div>
+                    <div class="imgpart" data-aos="zoom-in">
+                        <a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a>
                     </div>
                 </div>
-                
+
+                <div class="txtBox">
+                    <div class="txtcon"
+                        data-aos="fade-right">
+                        <h3>03</h3>
+                        <em>Title: . 인생을 관현악이며,<br>
+                            Use Language:오아이스도 위하여 <br>
+아름답고 미인을 동력은 있으며, 원대하고, 청춘의 청춘이 그들은 동산에는 것이다 물방아 눈이 부패뿐이다.  아름다우냐?</em>
+                    </div>
+                    <a href="#"> <button Type="button">자세히 보기</button></a>
+                </div>
+            </div>
         </Section>
+        <Section class="moretap">
+                <div class="moreBoxs clear">
+                    <div class="thinBoxG"></div>
+                    <div class="moreTxtBox">
+                        <a href="#">
+                            <b>More<br>view</b><br>
+                            <i>view</i>
+                        </a>
+                    </div>
+                    <div class="moreImg"><a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a></div><div class="moreImg"><a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a></div><div class="moreImg"><a href="#"><img src="/mainpofo/img/img_01.jpg" alt=""></a></div>
+                    <div class="thinBoxY"></div>
+                </div>
+        
+            <span class="moreborder"></span>
+
+        </Section>
+
         
     </div>
 
