754e81bc13dc105b7a95e7e9f681d722a8da1281
jung_jung <kiki128qk@naver.com>
2021-04-01 18:21:54 +0900

    04-01 W3C 통합 검사 후 수정완료

--- include/header.php
+++ include/header.php
@@ -8,7 +8,7 @@
 <div class="mainBoxs">
     <div class="boxL">
         <h1>panton<br>color<br>2021</h1>
-        <svg version="1.1" id="panBox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"y="0px" viewBox="0 0 450 450" style="enable-background:new 0 0 450 450;" xml:space="preserve">
+        <svg version="1.1" id="panBox" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 450 450" style="enable-background:new 0 0 450 450;" xml:space="preserve">
             <g>
                 <rect x="23.5" y="20" class="st0" width="257.5" height="254"/>
                 <path d="M276.5,25v248.3h-250V25H276.5 M301.5,0H1.5v298.3h300V0L301.5,0z"/>
@@ -23,7 +23,7 @@
     </div><div data-aos="fade-left"></div>
     <div class="boxR">
         <h2 class="pointtit" data-aos="fade-left">D</h2>
-        <h2 class="tittop" data-aos="fade-left">raw up a</h2><br><h2 class="titottom"data-aos="fade-left">raft Design</h2>
+        <h2 class="tittop" data-aos="fade-left">raw up a</h2><br><h2 class="titottom" data-aos="fade-left">raft Design</h2>
     </div>
 </div>
 <nav>
--- index.php
+++ index.php
@@ -163,7 +163,7 @@
             </div>
           </div>
         </div>
-        <!-- </div> -->
+        </div>
     </section>  
     <section class="contents" id="content">
       <h2>content</h2>
@@ -171,18 +171,18 @@
         <div class="info_con">
           <div class="info_txt">
             <h3>Pantone</h3>
-            <em>Use Language: <p>html</p>,<p>css</p>,<p>js</p>,<p>jquery</p></em><br> 
+            <em>Use Language: <b>html</b>,<b>css</b>,<b>js</b>,<b>jquery</b></em><br> 
               <i>올해의 인재상을 팬톤 2021년 컬러에 투영한 컨셉으로 제작 페이지</i>
           </div>
           <div class="info_buttons ">
-             <a href="javascript:void(window.open('https://github.com/kiki128qk/mainpofo/img/mainpofo.pdf', '_blank'))"> <button type="button"class="button">자세히 보기</button></a>
-             <a href="javascript:void(window.open('https://github.com/kiki128qk/mainPofo', '_blank'))"> <button type="button" class="button">Github</button></a>
-          </div>
+                <a class="button" href="javascript:void(window.open('https://github.com/kiki128qk/mainpofo/img/mainpofo.pdf', '_blank'))">자세히 보기</a>
+                  <a class="button" href="javascript:void(window.open('https://github.com/kiki128qk/mainPofo', '_blank'))">Github</a>
+              </div>
         </div> 
         <div class="pla_con " >
           <div class="pla_txt">
             <h3>UX/UI 기획</h3>
-            <em> Use Language: <p>php</p> , <p>css</p> , <p>js</p> , <p>jquery</p> , <p>mysql</p><br></em><br>
+            <em> Use Language: <b>php</b> , <b>css</b> , <b>js</b> , <b>jquery</b> , <b>mysql</b><br></em><br>
             <i>Nemesis 가상의 사업체의 사이트 UX/UI 기획 PROJECT</i>
             <b>- 분석, 설계, 구현으로 구성된 기획서</b>
             
@@ -193,16 +193,14 @@
               <button type="button" class="button cli_button" id="cliButton">자세히 보기</button>
               
               <div id="hideButtons" class="hide_buttons">
-                <a href="javascript:void(window.open('https://github.com/kiki128qk/mainpofo/img/ux_ui.pptx.pdf', '_blank'))">
-                  <button type="button" class="button hide_button">기획서</button>
-                </a>
-                <a href="#">
-                  <button type="button" class="button hide_button">페이지 가기</button>
+                <a class="button hide_button" href="javascript:void(window.open('https://github.com/kiki128qk/mainpofo/img/ux_ui.pptx.pdf', '_blank'))">기획서</a>
+                <a class="button hide_button" href="#">
+                  페이지 가기
                 </a>
               </div>
 
             </div>
-            <a href="#"><button type="button" class="button">Github</button></a>
+              <a class="button" href="#">Github</a>
           </div>
         </div>
       </div>
@@ -227,7 +225,7 @@
         <div class="txtBox">
           <div class="txtcon" data-aos="fade-right">
             <h3>Title: horizons </h3>
-              <em> Use Language:<p>php</p>,<p>css</p>,<p>js</p> ,<p>jquery</p>,<p>mysql</p><br></em><br>
+              <em> Use Language:<b>php</b>,<b>css</b>,<b>js</b> ,<b>jquery</b>,<b>mysql</b><br></em><br>
               <i> 프론트 앤드 개발 부터 서버 사이트 통합한 포괄적 기능의 사이트<br>
               데이터 입력 및 출력, 검색 전반적인 백앤드 기능 구현<br>
               어드민 기능으로 입력된 데이터의 검색, 수정 등 전반적인 관리</i>
@@ -235,20 +233,12 @@
           <div class="pla_buttons">
             <div class="pla_click" id="toggleBox02">
               <button type="button" class="button cli_button" id="cliButton">자세히 보기</button>
-              
               <div id="hideButtons" class="hide_buttons">
-                <a href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/mainpofo/img/horizons.pdf', ''))">
-                  <button type="button" class="button hide_button">기획서</button>
-                </a>
-                <a href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/gold/', ''))">
-                  <button type="button" class="button hide_button">페이지 가기</button>
-                </a>
+                  <a class="button hide_button" href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/mainpofo/img/horizons.pdf', ''))">기획서</a>
+                  <a class="button hide_button" href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/gold/', ''))">페이지 가기</a>
               </div>
-
             </div>
-            <a href="javascript:void(window.open('https://github.com/kiki128qk/gold', '_blank'))">
-              <button type="button" class="button">Github</button>
-            </a>
+              <a class="button" href="javascript:void(window.open('https://github.com/kiki128qk/gold', '_blank'))">Github</a>
           </div>
         </div>
       </div>
@@ -272,7 +262,7 @@
         <div class="txtBox">
           <div class="txtcon" data-aos="fade-right">
             <h3>Title: The Movie</h3>
-            <em>Use Language:<p>php</p>,<p>css</p>,<p>js</p>,<p>jquery</p><br></em>
+            <em>Use Language:<b>php</b>,<b>css</b>,<b>js</b>,<b>jquery</b><br></em>
             <i>애플리케이션 프로그래밍 인터페이스(AIP)을 활용해<br>
               외부에서 받은 데이터를 활용하여<br> 영화 검색,최신영화, 장르별 영화 정보 확인 가능
             </i>
@@ -280,20 +270,13 @@
           <div class="pla_buttons">
             <div class="pla_click" id="toggleBox03">
               <button type="button" class="button cli_button" id="cliButton">자세히 보기</button>
-              
               <div id="hideButtons" class="hide_buttons">
-                <a href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/mainpofo/img/the_movie.pdf', '_blank'))">
-                  <button type="button" class="button hide_button">기획서</button>
-                </a>
-                <a href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/movie_api/', '_blank'))">
-                  <button type="button" class="button hide_button">페이지 가기</button>
-                </a>
+                    <a class="button hide_button" href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/mainpofo/img/the_movie.pdf', '_blank'))">기획서</a>
+                    <a class="button hide_button" href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/movie_api/', '_blank'))">
+                  페이지 가기</a>
               </div>
-
             </div>
-            <a href="javascript:void(window.open('https://github.com/kiki128qk/movie_api', '_blank'))">
-              <button type="button" class="button">Github</button>
-            </a>
+              <a class="button" href="javascript:void(window.open('https://github.com/kiki128qk/movie_api', '_blank'))">Github</a>
           </div>
         </div>
       </div>
@@ -320,7 +303,7 @@
         <div class="txtBox">
           <div class="txtcon" data-aos="fade-right">
             <h3>Title: my schedule</h3>
-            <em>Use Language:<p>html</p>,<p>css</p>,<p>js</p>,<p>jquery</p>,<p>mysql</p></em>
+            <em>Use Language:<b>html</b>,<b>css</b>,<b>js</b>,<b>jquery</b>,<b>mysql</b></em>
             <i>mysql과 파이차트,grid를 활용하여 PORJUCT의 달성율 확인을 위한 페이지<br>각 카테고리별 완료 항목을 기록하여 자세한 스케줄 관리를 할수 있는 기능을 활용 할수 있음 <br>사용권한을 주기 위해 인증코드 입력후 이용가능</i>
           </div>
           <div class="pla_buttons">
@@ -328,18 +311,11 @@
               <button type="button" class="button cli_button" id="cliButton">자세히 보기</button>
               
               <div id="hideButtons" class="hide_buttons">
-                <a href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/mainpofo/img/my-schedule.pdf', '_blank'))">
-                  <button type="button" class="button hide_button">기획서</button>
-                </a>mainpofo/img/my-schedule.pdf
-                <a href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/myschedule/', '_blank'))">
-                  <button type="button" class="button hide_button">페이지 가기</button>
-                </a>
+                  <a class="button hide_button" href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/mainpofo/img/my-schedule.pdf', '_blank'))">기획서</a>
+                  <a class="button hide_button" href="javascript:void(window.open('http://kiki128qk.dothome.co.kr/myschedule/', '_blank'))">페이지 가기</a>
               </div>
-
             </div>
-            <a href="javascript:void(window.open('https://github.com/kiki128qk/my-schedule', '_blank'))">
-              <button type="button" class="button">Github</button>
-            </a>
+              <a class="button" href="javascript:void(window.open('https://github.com/kiki128qk/my-schedule', '_blank'))">Github</a>
           </div>
         </div>
       </div>
@@ -354,14 +330,10 @@
         </div>
         <div class="slider-box">
           <ul class="moreImg" id="more-img">
-            <li><img src="/mainpofo/img/img_01.jpg"></li>
-            <li><img src="/mainpofo/img/img_01.jpg"></li>
-            <li><img src="/mainpofo/img/img_01.jpg"></li>
-            <li><img src="/mainpofo/img/img_01.jpg"></li>
-            <li><img src="/mainpofo/img/img_01.jpg"></li>
-            <li><img src="/mainpofo/img/img_01.jpg"></li>
-            <li><img src="/mainpofo/img/img_01.jpg"></li>
-            <li><img src="/mainpofo/img/img_01.jpg"></li>
+            <li><img src="/mainpofo/img/ridibooks.png" alt="ridibooks_page_img"></li>
+            <li><img src="/mainpofo/img/ready.png" alt="ready_img"></li>
+            <li><img src="/mainpofo/img/ready.png" alt="ready_img"></li>
+            <li><img src="/mainpofo/img/ready.png" alt="ready_img"></li>
           </ul>
         </div>
         <!-- <div class="thinBoxY"></div> -->
